474c2e756b3b11c4618c20b09986c9d396ce8c71
Philippe Covington <philippehcov@gmail.com>
2022-07-22 10:43:39 -0400

    Exported

---
+++ my-app/src/components/RideList.jsx
@@ -0,0 +1,22 @@
+import ride from './seed'
+
+const RideList = (props) => {
+  
+  console.log(props)
+  return( 
+    <div className="grid">
+      {
+        props.rides.map((rides)=>(
+          <div key={ride.id}
+            className="card">
+            <h3>{ride.name}</h3>
+            <h4>{ride.category}</h4>
+            <h4>{ride.times_slot}</h4>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
+export default RideList
